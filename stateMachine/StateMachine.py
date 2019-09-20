467ed81c8f7a53d5b5be8bedfcf1014b69db0fb8
marius kaiser <kaisermj@mail.tu-berlin.de>
2019-09-20 16:22:36 +0200

    rem

--- stateMachine/StateMachine.py
+++
@@ -1,9 +0,0 @@
-
-
-class BeatDetector:
-    def __init__(self, state):
-        self.state = state
-
-    def changeState(new_state):
-        #send some stuff to dance loop
-        self.state = new_state
